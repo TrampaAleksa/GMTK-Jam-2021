86449e6e868e45eae384b46089193fec2b1f9739
urosspasenovic <urosspasenovic9@gmail.com>
2021-06-12 00:02:47 +0200

    Movement and switch characters on E

---
+++ Assets/URP/Scripts/Movement.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField]
+    float movementSpeed;
+    [SerializeField]
+    float turnSmoothTime = 0.1f;
+    
+    private float turnSmoothVelocity;
+    private Rigidbody _rb;
+    private Transform _cam;
+    private Transform _playerTransform;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _cam = _cam == null ? Camera.main.transform : _cam;
+        _playerTransform = transform;
+    }
+
+    private void FixedUpdate()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + _cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(_playerTransform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            _playerTransform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+
+            _rb.MovePosition(_playerTransform.position + moveDir.normalized * (movementSpeed * Time.deltaTime));
+
+        }
+    }
+}
---
+++ Assets/URP/Scripts/SwitchCharacters.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchCharacters : MonoBehaviour
+{
+    GameObject[] characters;
+    Movement[] movementScripts;
+    int i = 0;
+    private void Awake()
+    {
+        characters = GameObject.FindGameObjectsWithTag("Character");
+        movementScripts = new Movement[characters.Length];
+        for (int i = 0; i < characters.Length; i++)
+        {
+            movementScripts[i] = characters[i].GetComponent<Movement>();
+            //movementScripts[i].enabled = false;
+        }
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        movementScripts[0].enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            movementScripts[i++].enabled = false;
+            if (i == characters.Length)
+                i = 0;
+            movementScripts[i].enabled = true;
+        }
+    }
+}
