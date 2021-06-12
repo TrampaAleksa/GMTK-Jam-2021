c2f291a47e5dcc57b8dd7e47a3f1d16982da87c7
Aleksa Trampa <alektramp@gmail.com>
2021-06-12 13:11:08 +0200

    cleaned up win area script

--- Assets/_Project/Signals/SignalLineDrawer.cs
+++ Assets/_Project/Signals/SignalLineDrawer.cs
@@ -1,4 +1,6 @@
 ﻿
+using UnityEngine;
+
 namespace _Project.Signals
 {
     public class SignalLineDrawer
@@ -10,6 +12,14 @@ namespace _Project.Signals
         
             signal.line.SetPosition(0, broadcasterPosition);
             signal.line.SetPosition(1, receiverPosition);
+        }
+
+        public static bool IsInterrupted(Transform character1, Transform character2)
+        {
+            var receiverPosition = character1.position;
+            var broadcasterPosition = character2.position;
+
+            return Physics.Linecast(broadcasterPosition, receiverPosition, LayerMask.GetMask("Wall"));
         }
     }
 }
--- Assets/_Project/Win/WinArea.cs
+++ Assets/_Project/Win/WinArea.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Linq;
+using _Project.Signals;
 using UnityEngine;
 
 namespace _Project.Win
@@ -8,36 +10,57 @@ namespace _Project.Win
         public int numberToWin;
 
         private WinEvent[] _winEvents;
+        private Signal[] _signals;
         private int _charactersInArea;
 
         private void Awake()
         {
             _winEvents = GetComponentsInChildren<WinEvent>();
+            _signals = FindObjectsOfType<Signal>();
         }
 
         public void OnTriggerEnter(Collider other)
         {
             if (!other.gameObject.CompareTag("Character"))
                 return;
-            
-            
-            _charactersInArea++;
-            if (_charactersInArea < numberToWin)
-                return;
 
-            //TODO - Add signal not disconnected condition
-            foreach (var winEvent in _winEvents)
-            {
-                winEvent.Win();
-            }
+            _charactersInArea++;
         }
 
         public void OnTriggerExit(Collider other)
         {
             if (!other.gameObject.CompareTag("Character"))
                 return;
-            
+
             _charactersInArea--;
         }
+
+        private void Update()
+        {
+            if (IsWinConditionMet())
+                WinTheLevel();
+        }
+
+        private bool IsWinConditionMet()
+        {
+            if (!AllSignalsConnected) return false;
+            if (!AllCharactersInArea) return false;
+
+            return true;
+        }
+
+        private void WinTheLevel()
+        {
+            foreach (var winEvent in _winEvents)
+            {
+                winEvent.Win();
+            }
+
+            enabled = false;
+        }
+
+        private bool AllSignalsConnected => _signals.All(signal => signal.Connected);
+
+        private bool AllCharactersInArea => _charactersInArea >= numberToWin;
     }
 }
