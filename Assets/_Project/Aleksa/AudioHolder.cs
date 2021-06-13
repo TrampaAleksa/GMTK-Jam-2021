5752382be504c12eb8e399304baa07229a9b3831
Aleksa Trampa <alektramp@gmail.com>
2021-06-13 14:09:57 +0200

    Switch characters sound added

--- Assets/_Project/Aleksa/AudioHolder.cs
+++ Assets/_Project/Aleksa/AudioHolder.cs
@@ -29,5 +29,20 @@ namespace _Project.Aleksa
             }
             else Destroy(gameObject);
         }
+
+        private int _swapIndex;
+        public void SwapCharacters()
+        {
+            if (_swapIndex == 0)
+            {
+                SwitchCharacter1.Play();
+                _swapIndex = 1; 
+            }
+            else
+            {
+                SwitchCharacter2.Play();
+                _swapIndex = 0;
+            }
+        }
     }
 }
