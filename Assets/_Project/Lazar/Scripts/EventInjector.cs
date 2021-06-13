fd1d88a2d69b8ece42318e932865472a7a031fa1
Lazar Andric <Lazarandric97@gmail.com>
2021-06-13 18:51:06 +0200

    Add injector for event

---
+++ Assets/_Project/Lazar/Scripts/EventInjector.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventInjector : MonoBehaviour
+{
+    SceneHandler handler;
+    void Start()
+    {
+        handler = FindObjectOfType<SceneHandler>();
+    }
+
+    public void LoadScene(int id)
+    {
+        handler.LoadScene(id);
+    }
+    public void NextLevel()
+    {
+        var id = handler.GetId();
+        handler.LoadScene(id + 1);
+    }
+    public void RestartLevel()
+    {
+        var id=handler.GetId();
+        handler.LoadScene(id);
+    }
+
+
+}
