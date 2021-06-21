529dcd0d5775b14f8713d46b4761f53b516a7416
Aleksa Trampa <alektramp@gmail.com>
2021-06-21 18:07:10 +0200

    removed alarm instance reference from timer signal event

--- Assets/_Project/Aleksa/Signals/TimerSignalEvent.cs
+++ Assets/_Project/Aleksa/Signals/TimerSignalEvent.cs
@@ -1,19 +1,44 @@
 using _Project.Aleksa.LevelFinish;
+using UnityEngine;
 
 namespace _Project.Aleksa.Signals
 {
     public class TimerSignalEvent : SignalEvent
     {
+        private Alarm _alarm;
+
+        private void Awake()
+        {
+            _alarm = GetAlarm();
+        }
+
         public override void OnConnect(Signal signal)
         {
             base.OnConnect(signal);
-            Alarm.Instance.StopTimer();
+            _alarm.StopTimer();
         }
 
         public override void OnDisconnect(Signal signal)
         {
             base.OnDisconnect(signal);
-            Alarm.Instance.StartTimer();
+            _alarm.StartTimer();
+        }
+
+
+        private Alarm GetAlarm()
+        {
+            var alarm = FindObjectOfType<Alarm>();
+            
+            if (alarm == null)
+            {
+                Debug.LogWarning("No alarm component found, returning default implementation", this);
+
+                alarm = gameObject.AddComponent<Alarm>();
+                alarm.totalTime = 99;
+                alarm.timeFactor = 1f;
+            }
+            
+            return alarm;
         }
     }
 }
