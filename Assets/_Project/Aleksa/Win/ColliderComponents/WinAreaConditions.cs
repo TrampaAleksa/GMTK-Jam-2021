68a80d5c6b71a0fdccc00abee3be3c36b48f6618
Aleksa Trampa <alektramp@gmail.com>
2021-06-23 00:40:23 +0200

    added condition met logic into win area conditions script

--- Assets/_Project/Aleksa/Win/ColliderComponents/WinAreaConditions.cs
+++ Assets/_Project/Aleksa/Win/ColliderComponents/WinAreaConditions.cs
@@ -1,9 +1,40 @@
+using System.Linq;
+using _Project.Aleksa.Signals;
 using UnityEngine;
 
 namespace _Project.Aleksa.Win.ColliderComponents
 {
     public class WinAreaConditions : MonoBehaviour
     {
+        public int charactersToWin;
         
+        private int _charactersInArea;
+        private Signal[] _signals;
+
+        private void Awake()
+        {
+            _signals = FindObjectsOfType<Signal>();
+        }
+        
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("Character"))
+                _charactersInArea++;
+        }
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.gameObject.CompareTag("Character"))
+                _charactersInArea--;
+        }
+
+        public bool IsWinConditionMet()
+        {
+            if (!AllSignalsConnected) return false;
+            if (!AllCharactersInArea) return false;
+
+            return true;
+        }
+        private bool AllSignalsConnected => _signals.All(signal => signal.Connected);
+        private bool AllCharactersInArea => _charactersInArea >= charactersToWin;
     }
 }
