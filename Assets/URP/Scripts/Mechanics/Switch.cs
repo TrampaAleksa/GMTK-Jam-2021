d150200c84327e370ebee6d1debce1ca4db33f00
urosspasenovic <urosspasenovic9@gmail.com>
2021-10-03 16:16:25 +0200

    Fixed switch
    Now 2 characters can stand next to switch and activate it.

--- Assets/URP/Scripts/Mechanics/Switch.cs
+++ Assets/URP/Scripts/Mechanics/Switch.cs
@@ -8,20 +8,17 @@ public class Switch : MonoBehaviour
     MoveWall[] wallsToOpen;
 
 
-    GameObject character;
+    List<GameObject> character = new List<GameObject>();
     private bool thereIsWallWithStartingPositionDown;
-    private bool canSwitch = false;
     private int n = 2;
 
     private void OnCollisionEnter(Collision collision)
     {
-        character = collision.gameObject;
-        canSwitch = true;
+        character.Add(collision.gameObject);
     }
     private void OnCollisionExit(Collision collision)
     {
-        character = null;
-        canSwitch = false;
+        character.Remove(collision.gameObject);
     }
     private void Awake()
     {
@@ -29,43 +26,45 @@ public class Switch : MonoBehaviour
     }
     private void Update()
     {
-        if (canSwitch)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                ActivateSwitch();
-            }
+            ActivateSwitch();
         }
     }
     private void ActivateSwitch()
     {
-        if (character != null)
+        if (character.Count != 0)
         {
-            if (character.GetComponent<Movement>().enabled)
+            foreach (GameObject robot in character)
             {
-                if (thereIsWallWithStartingPositionDown)
+                if (robot.GetComponent<Movement>().enabled)
                 {
-                    for (int i = 0; i < wallsToOpen.Length; i++)
+                    if (thereIsWallWithStartingPositionDown)
                     {
-                        // On every click adds (-1)^n, on first click it will add 1 to every wall its atached and walls will go down
-                        // on second click it will add -1 to every wall and so on.
-                        if (wallsToOpen[i].startingPositionIsDown)
+                        for (int i = 0; i < wallsToOpen.Length; i++)
                         {
-                            wallsToOpen[i].numberOfObjectsThatAffectsWall += Mathf.Pow(-1, n - 1);
-                            continue;
+                            // On every click adds (-1)^n, on first click it will add 1 to every wall its atached and walls will go down
+                            // on second click it will add -1 to every wall and so on.
+                            if (wallsToOpen[i].startingPositionIsDown)
+                            {
+                                wallsToOpen[i].numberOfObjectsThatAffectsWall += Mathf.Pow(-1, n - 1);
+                                continue;
+                            }
+                            wallsToOpen[i].numberOfObjectsThatAffectsWall += Mathf.Pow(-1, n);
                         }
-                        wallsToOpen[i].numberOfObjectsThatAffectsWall += Mathf.Pow(-1, n);
                     }
-                }
-                else
-                {
-                    for (int i = 0; i < wallsToOpen.Length; i++)
+                    else
                     {
-                        wallsToOpen[i].numberOfObjectsThatAffectsWall += Mathf.Pow(-1, n);
+                        for (int i = 0; i < wallsToOpen.Length; i++)
+                        {
+                            wallsToOpen[i].numberOfObjectsThatAffectsWall += Mathf.Pow(-1, n);
+                        }
                     }
+                    n++;
+                    break;
                 }
-                n++;
             }
+            
         }
     }
     private void ThereIsWallWithStartingPositionDown()
